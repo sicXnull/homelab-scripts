84ee71626a6fb634af6efd70c491435f55c9d2e6
sicXnull <31908995+sicXnull@users.noreply.github.com>
2023-04-09 13:42:36 -0400

    add notifications if expiring soon and expired.

--- certbot-renewal.py
+++ certbot-renewal.py
@@ -6,11 +6,19 @@ import re
 webhook_url = "https://discord.com/api/webhooks/<embed>>"
 
 # Discord embed message
-def send_discord_message(expiration_date):
+def send_discord_message(title, description, expiration_date):
+    if title == "Certificate Renewed":
+        color = 3066993  # Green
+    elif title == "Certificate Expiring Soon":
+        color = 15844367  # Yellow
+    else:
+        title = "Certificate Expired!"
+        color = 15158332  # Red
+
     embed = {
-        "title": "Certificate Renewed",
-        "description": f"Certificate for {base_domain} has been renewed",
-        "color": 3066993,
+        "title": title,
+        "description": description,
+        "color": color,
         "fields": [
             {
                 "name": "Expiration Date",
@@ -19,12 +27,27 @@ def send_discord_message(expiration_date):
         ],
         "thumbnail": {
             "url": "https://i.imgur.com/aj1M1iz.png"
-        }
+            }
     }
-    requests.post(webhook_url, json={'embeds': [embed]})
+    json = {'embeds': [embed]}
+    response = requests.post(webhook_url, json=json)
+    print(response.status_code)
+    print(response.content)
+
+
+# Check if certificate is expiring soon or has expired and send notification
+def check_certificate_expiry(expiration_date):
+    expiry_datetime = datetime.strptime(expiration_date, "%b %d %H:%M:%S %Y %Z")
+    warning_period = timedelta(days=15)  # Notify if certificate expires within 15 days
+    if expiry_datetime - datetime.now() <= timedelta(0):
+        send_discord_message("Certificate Expired", f"Wildcard certificate for {base_domain} has expired.", expiration_date)
+    elif expiry_datetime - datetime.now() <= warning_period:
+        send_discord_message("Certificate Expiring Soon", f"Wildcard certificate for {base_domain} will expire on {expiration_date}.", expiration_date)
+    else:
+        send_discord_message("Certificate Renewed", f"Wildcard certificate for {base_domain} has been renewed.", expiration_date)
+
 
 # Get current expiration date
-
 subdomain = ""
 base_domain = "whatever.com"
 if subdomain:
@@ -42,10 +65,9 @@ try:
 except FileNotFoundError:
     previous_expiration_date = ''
 
-# Compare expiration dates
+# Compare expiration dates and send notification if necessary
 if expiration_date != previous_expiration_date:
-    # Send Discord message
-    send_discord_message(expiration_date)
+    check_certificate_expiry(expiration_date)
     # Save new expiration date
     with open('expiration_date.txt', 'w') as file:
         file.write(expiration_date)
