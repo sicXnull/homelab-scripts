da67cfc00b75bdab4bf69c5290c7806c043735e6
sicXnull <31908995+sicXnull@users.noreply.github.com>
2023-05-23 18:59:55 -0400

    Handle Error 23 (partial transfer) as success. Fix Logging

--- rsync.py
+++ rsync.py
@@ -4,23 +4,32 @@ import requests
 import re
 import time
 import subprocess
-import datetime
+from datetime import datetime
 import pytz
 
-start_time = time.time()
+script_dir = os.path.dirname(os.path.abspath(__file__))
+log_file_path = os.path.join(script_dir, "rsync.log")
 
 source_dirs = ["/opt", "/home"]
 target_dir = "/NAS"
 webhook_url = 'https://discord.com/api/webhooks/<embed>'
+
+start_time = time.time()
 hostname = os.uname()[1]
 source_dirs_string = " ".join(source_dirs)
 
-with open("rsync.log", "w") as log_file:
+# Create the rsync.log file if it doesn't exist
+if not os.path.exists(log_file_path):
+    with open(log_file_path, "w") as log_file:
+        pass
+
+with open(log_file_path, "a") as log_file:
     result = subprocess.run(
-        f"rsync -rltvz -a --no-links --no-specials --no-devices --delete --stats --info=skip0 {source_dirs_string} {target_dir}",
+        f"rsync -rltvz -a --no-links --no-specials --no-devices --no-inc-recursive --delete --stats --info=progress2 --verbose {source_dirs_string} {target_dir}",
         shell=True,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
+
     )
     output = result.stdout.decode()
     log_file.write(output)
@@ -33,14 +42,18 @@ if match:
     created = match.group(2)
     deleted = match.group(3)
     transferred = match.group(4)
-
 else:
     synced = 0
     created = 0
     deleted = 0
     transferred = 0
 
-# Embed
+print(synced, created, deleted, transferred)
+
+# Handle Error 23 (partial transfer) as success
+if result.returncode == 23:
+    result.returncode = 0
+
 if result.returncode == 0:
     duration_seconds = time.time() - start_time
     duration_minutes, duration_seconds = divmod(duration_seconds, 60)
@@ -81,7 +94,7 @@ if result.returncode == 0:
         'color': 12868102
     }
 else:
-    with open("rsync.log", "r") as file:
+    with open(log_file_path, "r") as file:
         error_output = file.read()
         error_output = error_output[-2000:]
         embed = {
